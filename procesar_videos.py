7431779eb0cedda0a15678cb14e0bb1aadb78c0a
isaacsaltiel <63256229+isaacsaltiel@users.noreply.github.com>
2025-07-17 21:43:55 -0600

    Update procesar_videos.py

--- procesar_videos.py
+++ procesar_videos.py
@@ -92,7 +92,7 @@ for video in videos_nuevos:
     url_cloudinary = f"https://res.cloudinary.com/{CLOUD_NAME}/video/upload"
     url_cloudinary += f"/l_puntazo_video,w_0.70/fl_layer_apply,g_north_west,x_30,y_30"
     if segundo_logo_existe:
-        url_cloudinary += f"/l_{logo_overlay_id},w_0.3/fl_layer_apply,g_north_east,x_50,y_50"
+        url_cloudinary += f"/l_{logo_overlay_id},w_0.5/fl_layer_apply,g_north_east,x_50,y_50"
     url_cloudinary += f"/q_auto,f_mp4/videos_con_marca/{base_name}.mp4"
 
     # 3. Subir video a Cloudinary
