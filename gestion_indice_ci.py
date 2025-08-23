10591af0f35aa5e82f88a143951ae85ce22ef016
isaacsaltiel <63256229+isaacsaltiel@users.noreply.github.com>
2025-08-23 11:57:46 -0600

    Update gestion_indice_ci.py

--- gestion_indice_ci.py
+++ gestion_indice_ci.py
@@ -14,7 +14,7 @@ GITHUB_TOKEN = os.environ.get("PAT_GITHUB")
 
 # Configuraciones globales
 VALID_SUFFIX = ".mp4"
-RETENTION_HOURS = 168
+RETENTION_HOURS = 24
 DROPBOX_BASE = "/Puntazo/Locaciones"
 GITHUB_REPO = "isaacsaltiel/puntazo_web_v2"
 
